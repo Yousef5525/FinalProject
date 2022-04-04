179fa93476efbd7108410204c43d72d393cf0bfa
يوسف علاء عبده <joealaa552005@gmail.com>
2022-04-04 17:21:27 +0300

    ...

--- Final Project/assests/JS/Books.js
+++ Final Project/assests/JS/Books.js
@@ -1,9 +1,19 @@
-var hovering = function () {
-  $("ul").show("slide", { direction: "right" }, 1000);
-};
-
-var leaving = function () {
-  $("ul").hide("slide", { direction: "left" }, 1000);
-};
-
-$("container").hover(hovering, leaving);
+function ChangeBox(type) {
+  if (type == "history") {
+    document.getElementById("history").style.display = "flex";
+    document.getElementById("novel").style.display = "none";
+    document.getElementById("islam").style.display = "none";
+  } else if (type == "novel") {
+    document.getElementById("history").style.display = "none";
+    document.getElementById("novel").style.display = "flex";
+    document.getElementById("islam").style.display = "none";
+  } else if (type == "islam") {
+    document.getElementById("history").style.display = "none";
+    document.getElementById("novel").style.display = "none";
+    document.getElementById("islam").style.display = "flex";
+  } else if (type == "all") {
+    document.getElementById("history").style.display = "flex";
+    document.getElementById("novel").style.display = "flex";
+    document.getElementById("islam").style.display = "flex";
+  }
+}
